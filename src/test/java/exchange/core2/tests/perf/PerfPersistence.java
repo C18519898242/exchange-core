4685d5aea55d3f181b448122c2ff2a4c3327105f
c18519898242 <15342660+c18519898242@user.noreply.gitee.com>
2025-08-15 18:17:10 +0800

    这个异常 et.openhft.chronicle.core.io.ThreadingIllegalStateException 是 Chronicle 库的开发者在新版本中特意增加的一个安全保护机制。

--- src/test/java/exchange/core2/tests/perf/PerfPersistence.java
+++ src/test/java/exchange/core2/tests/perf/PerfPersistence.java
@@ -128,7 +128,7 @@ public final class PerfPersistence {
                 TestDataParameters.localTestBuilder()
                         .preFillMode(TestOrdersGeneratorConfig.PreFillMode.ORDERS_NUMBER_PLUS_QUARTER)
                         .build(),
-                3);
+                1);
     }
 
 
